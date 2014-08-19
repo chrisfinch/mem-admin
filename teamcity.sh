2100a550365ac9274b60bc9317040c47dec260bf
Chris Finch <chris.finch@guardian.co.uk>
2014-08-19 12:38:40 +0100

    update tc script

--- teamcity.sh
+++ teamcity.sh
@@ -6,8 +6,14 @@ mkdir public/uploads
 
 mkdir -p target/packages/eventbriteImages
 
+zip -rv target/packages/eventbriteImages/app.zip bin config modules node_modules public routes views app.js package.json server.sh
+
 cp deploy.json target/
 
-zip -rv target/packages/eventbriteImages/app.zip .
+pushd target
 
-echo "##teamcity[publishArtifacts '.']"
+zip -rv artifacts.zip .
+
+popd
+
+echo "##teamcity[publishArtifacts '$(pwd)/target/artifacts.zip => .']"
