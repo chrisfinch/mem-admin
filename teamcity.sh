0bff7ba582077a8bdb5ef54001faf23d32b2079e
Paul Brown <paul.brown@guardian.co.uk>
2017-11-21 14:37:16 +0000

    Hardcoding the build node version to be the same as the deployment target.

--- teamcity.sh
+++ teamcity.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-####################################################################################################################
-#### Turned off because a load of unnecessary babel dependencies get installled and slow down deployments ##########
-####################################################################################################################
 #Install node v4.8.6 (same as the destination server)
-#NODE_VERSION="4.8.6"
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-#nvm install ${NODE_VERSION}
-#nvm use ${NODE_VERSION}
+NODE_VERSION="4.8.6"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
 
 npm install
 npm run riffraff
